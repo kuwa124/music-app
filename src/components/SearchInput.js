51bb88ff26d4a78d52e5aa88bc6ddca4571b8273
kuwa124 <c0mwgave1407@gmail.com>
2024-05-31 16:24:00 +0900

    💡 コメントを追加

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,13 +1,22 @@
+// FontAwesomeIconコンポーネントをインポートします。ここでは検索アイコンを使用するために、必要なアイコンを読み込んでいます。
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+// SearchInputコンポーネントを定義します。このコンポーネントは検索入力フィールドとボタンを提供します。
 export function SearchInput(props) {
+  // セクションコンポーネントを返します。セクションには検索入力フィールドとボタンが表示されます。
   return (
     <section className="mb-10">
+      {/* 検索入力フィールドを表示します。
+          クラス名を使用して、スタイルを適用しています。
+          プレースホルダーテキストを設定しています。 */}
       <input
         className="bg-gray-700 w-1/3 p-2 rounded-l-lg focus:outline-none"
         placeholder="探したい曲を入力してください"
       />
+      {/* 検索ボタンを表示します。
+          クラス名を使用して、スタイルを適用しています。
+          FontAwesomeIconコンポーネントを使用して、検索アイコンを表示しています。 */}
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r-lg">
         <FontAwesomeIcon icon={faSearch} />
       </button>
--- src/components/player.js
+++ src/components/player.js
@@ -1,24 +1,39 @@
+// FontAwesomeのアイコンをインポート
 import { faPlayCircle, faStopCircle } from '@fortawesome/free-solid-svg-icons';
+// FontAwesomeIconコンポーネントをインポート
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+// Playerコンポーネントを定義します。このコンポーネントは曲の情報を表示し、再生/停止ボタンを提供します。
 export function Player(props) {
+  // フッターコンポーネントを返します。フッターには曲の情報と再生/停止ボタンが表示されます。
   return (
     <footer className="fixed bottom-0 w-full bg-gray-800 p-5">
+      {/* グリッドレイアウトを使用して、3つのカラムを作成します。 */}
       <div className="grid grid-cols-3">
+        {/* 曲の情報を表示するカラム */}
         <div className="flex items-center">
+          {/* アルバムのサムネイル画像を表示します。 */}
           <img
+            // アルバムの画像を表示
             src={
               props.song.album.images[0].url
             }
+            // 代替テキストとCSSクラスを設定
             alt="thumbnail"
             className="rounded-full mr-3 h-[50px] w-[50px]"
           />
+          {/* 曲名とアーティスト名を表示します。 */}
           <div>
             <p className="text-sm font-semibold">{props.song.name}</p>
             <p className="text-xs text-gray-400">{props.song.artists[0].name}</p>
           </div>
         </div>
+        {/* 再生/停止ボタンを表示するカラム */}
         <div className="flex items-center justify-center">
+          {/* FontAwesomeIconコンポーネントを使用して、再生/停止ボタンを表示します。
+              ボタンのクリックイベントは、親コンポーネントから渡されたonButtonClickプロパティにバインドされます。
+              アイコンは、親コンポーネントから渡されたisPlayプロパーティに基づいて切り替わります。
+              曲がプレビュー可能な場合はボタンがクリック可能になり、そうでない場合は非アクティブになります。 */}
           <FontAwesomeIcon
             onClick={props.onButtonClick}
             icon={props.isPlay ? faStopCircle : faPlayCircle}
