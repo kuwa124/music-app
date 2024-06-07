4eaf06408e3f752cf40c4c3683024bff067467e7
kuwa124 <c0mwgave1407@gmail.com>
2024-06-07 12:34:47 +0900

    ♻️ 定数のエクスポートファイル作成

---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+// 1ページあたりの曲の数
+export const SONGS_PER_PAGE = 20;
+
+// APIのベースURL
+export const API_BASE_URL = 'https://api.spotify.com/v1';
+
+//  アプリ名
+export const APP_NAME = 'Music App';
+
+// デフォルトの画像URL
+export const DEFAULT_SONG_IMAGE = 'path/to/default-song-image.png';
+
+// アニメーションの持続時間
+export const ANIMATION_DURATION = 300; // ミリ秒
